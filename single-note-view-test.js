f33f8d5d9c4b91e8059a9fc5e8471eb6adf2d40c
Marcellofabbri <marcellogfabbri@gmail.com>
2020-02-05 01:11:26 +0000

    test driven instantiation method with a Note object and the HTML rendering of such note's body

--- single-note-view-test.js
+++ single-note-view-test.js
@@ -1,5 +1,15 @@
 function testSingleNoteViewInstantiation() {
-    var snv = new SingleNoteView(note);
+    var snv = new SingleNoteView("the body of a note");
     assert.isTrue(snv.note instanceof Note)
+    assert.isTrue(snv.note.text === "the body of a note")
 };
 testSingleNoteViewInstantiation();
+
+function testRenderNote() {
+    var snv = new SingleNoteView("the body of a note");
+    var document2 = document.implementation.createHTMLDocument('')
+    snv.render(document2, "div")
+    element = document2.getElementsByTagName("div")[0]
+    assert.isTrue(element.outerHTML === "<" + "div" + ">the body of a note</" + "div" + ">")
+};
+testRenderNote();
--- single-note-view.js
+++ single-note-view.js
@@ -1,6 +1,16 @@
+(function(exports) {
+    function SingleNoteView(body) {
+      this.note = new Note(body);
+    };
+  
+    exports.SingleNoteView = SingleNoteView;
+  })(this);
 
-
-
+SingleNoteView.prototype.render = function(doc = document, element = "div") {
+  el = doc.createElement(element)
+  el.innerText = this.note.text
+  doc.body.append(el)
+}
 
 
 var assert = {
